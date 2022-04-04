b0c761b025df06db0fb8ac5b5461589dcb15be8b
JimmyyNguyenn <101150721+JimmyyNguyenn@users.noreply.github.com>
2022-04-03 22:10:15 -0400

    Add files via upload

---
+++ 4_Motor_Relay.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(11, OUTPUT);    //Set the pin 11 as an OUTPUT
+}
+
+void loop() {
+  digitalWrite(11, HIGH); //Turn ON the pin 11
+  delay(2000);            //Set a delay of 2 seconds
+  digitalWrite(11, LOW);  //Turn OFF the pin 11
+  delay(2000):            //Set a delay of 2 seconds
+  }
